8d31851ffb6b55c64c966500cb96f19cb045f43d
Molotov <irfaankhan2008@gmail.com>
2026-02-19 14:29:31 -0500

    Update TurretOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TurretOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TurretOpMode.java
@@ -3,11 +3,15 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 import org.firstinspires.ftc.teamcode.classes.TurretMechanism;
 
 @TeleOp(name="Tune Turret PD")
 public class TurretOpMode extends OpMode {
 
+    DcMotorEx frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor;
     private TurretMechanism turret = new TurretMechanism();
     private Limelight3A limelight;
 
@@ -17,7 +21,12 @@ public class TurretOpMode extends OpMode {
     @Override
     public void init() {
         turret.init(hardwareMap, telemetry);
+        frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backRightMotor = hardwareMap.get(DcMotorEx.class, "backRight");
+        backLeftMotor = hardwareMap.get(DcMotorEx.class, "backLeft");
 
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
@@ -49,6 +58,25 @@ public class TurretOpMode extends OpMode {
             turret.setD(turret.getKD() - stepSizes[stepIndex]);
         }
 
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio,
+        // but only if at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
         // start it up boi
         turret.update(true);
         telemetry.addData("P Gain", turret.getKP());
